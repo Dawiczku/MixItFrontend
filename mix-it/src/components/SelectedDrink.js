ceead70ae1a9ae0613f2eb90e22058bb67891e09
Dawiczku <skarupski.dawid@gmail.com>
2024-05-13 22:37:27 +0200

    fix: fix the styling of hamburger and drink

--- mix-it/src/components/SelectedDrink.js
+++ mix-it/src/components/SelectedDrink.js
@@ -10,27 +10,27 @@ function SelectedDrink() {
   useEffect(() => {
     const fetchDrinkById = async () => {
       try {
-        const drinkData = await drinkService.getDrinkById(id);
-        // const drinkData = {
-        //   _id: {
-        //     $oid: "66100646fe2ccf09c43c9985",
-        //   },
-        //   name: "modzajtko version 2",
-        //   flavor: "SWEET",
-        //   baseAlcohol: "RUM",
-        //   ingredients: [
-        //     "świeża mięta",
-        //     "cukier trzcinowy",
-        //     "sok z limonki",
-        //     "soda",
-        //     "biały rum",
-        //   ],
-        //   strength: 8,
-        //   howToMake:
-        //     "W dużym kieliszku zgnieść 6-8 listków mięty z 2 łyżeczkami cukru. Dodać sok z limonki i rozpuścić cukier. Dodać kilka kostek lodu i wlać rum. Dopełnić napojem gazowanym. Wymieszać i udekorować gałązką mięty i plasterkiem limonki.",
-        //   rating: 5,
-        //   _class: "com.ZapitaTech.MixIt.Recipes.Recipe",
-        // };
+        // const drinkData = await drinkService.getDrinkById(id);
+        const drinkData = {
+          _id: {
+            $oid: "66100646fe2ccf09c43c9985",
+          },
+          name: "modzajtko version 2",
+          flavor: "SWEET",
+          baseAlcohol: "RUM",
+          ingredients: [
+            "świeża mięta",
+            "cukier trzcinowy",
+            "sok z limonki",
+            "soda",
+            "biały rum",
+          ],
+          strength: 8,
+          howToMake:
+            "W dużym kieliszku zgnieść 6-8 listków mięty z 2 łyżeczkami cukru. Dodać sok z limonki i rozpuścić cukier. Dodać kilka kostek lodu i wlać rum. Dopełnić napojem gazowanym. Wymieszać i udekorować gałązką mięty i plasterkiem limonki.",
+          rating: 5,
+          _class: "com.ZapitaTech.MixIt.Recipes.Recipe",
+        };
 
         setDrink(drinkData);
       } catch (error) {
@@ -48,7 +48,9 @@ function SelectedDrink() {
       <fieldset className="selected-drink selected-drink__ingredients">
         <legend>Skladniki</legend>
         {drink && (
-          <ol className={`ingredients-list ${drink.ingredients.length > 5 ? 'multi-column' : ''}`}>
+          <ol
+            className={`ingredients-list ${drink.ingredients.length > 5 ? "multi-column" : ""}`}
+          >
             {drink.ingredients.map((ingredient, index) => (
               <li key={index}>
                 <span className="dash">-</span> {ingredient}
@@ -58,20 +60,15 @@ function SelectedDrink() {
         )}
       </fieldset>
       <fieldset className="selected-drink selected-drink__photo">
-        <legend>Zdjecie</legend>
+        <legend>{drink.name}</legend>
         <p>PHOTO</p>
       </fieldset>
       <fieldset className="selected-drink selected-drink__recipe">
         <legend>Przepis</legend>
         {drink && <p>{drink.howToMake}</p>}
       </fieldset>
-      <fieldset className="selected-drink selected-drink__name">
-        <legend>Nazwa</legend>
-        {drink && <p>{drink.name}</p>}
-      </fieldset>
     </div>
   );
-  
 }
 
 export default SelectedDrink;
