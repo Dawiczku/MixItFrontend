ff025ab2021e62378ed3f0afac0c6a221139a851
Bartek <ostrycharz.bartlomiej@gmail.com>
2024-05-15 20:25:55 +0200

    Fixed image display

--- mix-it/src/components/SelectedDrink.js
+++ mix-it/src/components/SelectedDrink.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "../styles/SelectedDrink.css";
@@ -8,32 +7,10 @@ function SelectedDrink() {
   const { id } = useParams();
   const [drink, setDrink] = useState(null);
 
-
   useEffect(() => {
     const fetchDrinkById = async () => {
       try {
         const drinkData = await drinkService.getDrinkById(id);
-        // const drinkData = {
-        //   _id: {
-        //     $oid: "66100646fe2ccf09c43c9985",
-        //   },
-        //   name: "modzajtko version 2",
-        //   flavor: "SWEET",
-        //   baseAlcohol: "RUM",
-        //   ingredients: [
-        //     "świeża mięta",
-        //     "cukier trzcinowy",
-        //     "sok z limonki",
-        //     "soda",
-        //     "biały rum",
-        //   ],
-        //   strength: 8,
-        //   howToMake:
-        //     "W dużym kieliszku zgnieść 6-8 listków mięty z 2 łyżeczkami cukru. Dodać sok z limonki i rozpuścić cukier. Dodać kilka kostek lodu i wlać rum. Dopełnić napojem gazowanym. Wymieszać i udekorować gałązką mięty i plasterkiem limonki.",
-        //   rating: 5,
-        //   _class: "com.ZapitaTech.MixIt.Recipes.Recipe",
-        // };
-
         setDrink(drinkData);
       } catch (error) {
         console.error("Error fetching drink details:", error);
@@ -50,9 +27,7 @@ function SelectedDrink() {
       <fieldset className="selected-drink selected-drink__ingredients">
         <legend>Skladniki</legend>
         {drink && (
-          <ol
-            className={`ingredients-list ${drink.ingredients.length > 5 ? "multi-column" : ""}`}
-          >
+          <ol className={`ingredients-list ${drink.ingredients.length > 5 ? "multi-column" : ""}`}>
             {drink.ingredients.map((ingredient, index) => (
               <li key={index}>
                 <span className="dash">-</span> {ingredient}
@@ -63,8 +38,8 @@ function SelectedDrink() {
       </fieldset>
       <fieldset className="selected-drink selected-drink__photo">
         <legend>{drink && drink.name}</legend>
-        <div className="selected-drink__wrapper">
-         <img src={drink && drink.imageUrl} />
+        <div className="selected-drink__image-wrapper">
+          <img src={drink && drink.imageUrl} alt={drink && drink.name} className="selected-drink__image" />
         </div>
       </fieldset>
       <fieldset className="selected-drink selected-drink__recipe">
